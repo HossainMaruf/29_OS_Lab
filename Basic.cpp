6cef56e299cc199f54239854749740a5d8c67635
HossainMaruf <maruficepustian@gmail.com>
2024-01-09 14:51:10 +0600

    Initial

---
+++ Basic.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+  long long id;
+  string name, fName, mName;
+
+  cin >> id >> name >> fName >> mName;
+  cout << id <<" "<< name << " " << fName << " " << mName;
+
+
+
+  return 0;
+}
