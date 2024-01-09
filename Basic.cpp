f55e7ceb76abf562b644365040947a9e655d87de
HossainMaruf <maruficepustian@gmail.com>
2024-01-09 15:46:52 +0600

    Custom comparator for person

--- Basic.cpp
+++ Basic.cpp
@@ -6,12 +6,19 @@ struct Person {
   string name, fName, mName;
 };
 
+bool comp(struct Person p1, struct Person p2) {
+  if(p1.id < p2.id) return false;
+  else return true;
+}
+
 int main() {
 
   struct Person p[3]; 
 
   for(int i=0; i<3; i++)
     cin >> p[i].id >> p[i].name >> p[i].fName >> p[i].mName;
+
+  sort(p, p+3, comp);
 
   for(int i=0; i<3; i++)
     cout << p[i].id <<" "<< p[i].name << " " << p[i].fName << " " << p[i].mName << endl;
