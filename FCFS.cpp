99dacdcb79ba3989b09435f0b6f58cbeabbca223
HossainMaruf <maruficepustian@gmail.com>
2024-01-23 14:57:33 +0600

    FCFS Almost done

--- FCFS.cpp
+++ FCFS.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 struct Process {
-    int id, at, bt, st, ct;
+    int id, at, bt, st, ct, wt, tt;
 };
 bool comp(struct Process p1, struct Process p2) {
     if(p1.at < p2.at) return true;
@@ -14,15 +14,15 @@ void printProcess(struct Process p[], int n) {
     }
 }
 void printTable(struct Process p[], int n) {
-    cout << "ID\tAT\tBT\tST\tCT" << endl;
+    cout << "ID\tAT\tBT\tST\tCT\tWT\tTT" << endl;
     for(int i=0; i<n; i++) {
-        cout << p[i].id << "\t" << p[i].at << "\t" << p[i].bt << "\t" << p[i].st << "\t" << p[i].ct << endl;
+        cout << p[i].id << "\t" << p[i].at << "\t" << p[i].bt << "\t" << p[i].st << "\t" << p[i].ct << "\t" << p[i].wt << "\t" << p[i].tt << endl;
     }
 }
 
 int main() {
     freopen("input.txt", "r", stdin);
-    int n;
+    int n, sum_wt = 0;
     cin >> n;
     struct Process p[n];
     // Taking input
@@ -51,10 +51,16 @@ int main() {
             p[i].st = p[i-1].ct;
             p[i].ct = p[i].st + p[i].bt;
         }
+        p[i].wt = p[i].st - p[i].at;
+        p[i].tt = p[i].wt + p[i].bt;
+        sum_wt += p[i].wt;
    }
 
    // Printing the table
    printTable(p, n);
+
+   cout << endl;
+   cout << "AWT = " << (float)sum_wt / n << endl; 
   
     return 0;
 }
