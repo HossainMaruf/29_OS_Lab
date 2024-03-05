8dd71a7e5bc6e3ca7a1b1a33499c744a5c37f777
HossainMaruf <maruficepustian@gmail.com>
2024-03-05 16:48:24 +0600

    Bug in Best Fit

--- BestFit.cpp
+++ BestFit.cpp
@@ -24,17 +24,38 @@ int main()
     for(int i=0; i<nf; i++) {
         cin >> files[i];
     }
-    // FIRST FIT ALGORITHM
+    // BEST FIT ALGORITHM
     for(int i=0; i<nf; i++) {
         int file_size = files[i];
+        // Traverse the blocks array
+        vector<struct Blocks> v; 
         for(int j=0; j<nb; j++) {
-           if((blocks[j].block_size >= file_size) && (blocks[j].file_no == 0)) {
-            // allocated
-            blocks[j].file_no = i+1;
-            blocks[j].file_size = file_size;
-            blocks[j].fragmentation = blocks[j].block_size - file_size;
-            break;
-           } 
+            if((blocks[j].block_size >= file_size) && (blocks[j].file_no == 0)) {
+               v.push_back(blocks[j]);
+            }
+        }
+        cout << "File " << i+1 << endl;
+        for(int t=0; t<v.size(); t++) {
+            cout << v[t].block_no << " ";
+        }
+        cout << "END" << endl; 
+        // Which block is Minimum
+        int minIndex = 0;
+        for(int m=1; m<v.size(); m++) {
+        if(v[m].block_size <= v[minIndex].block_size) {
+            minIndex = m;
+        }
+        }
+
+        if(i == 1) {
+            cout << minIndex << endl;
+        }
+     
+        //Occupy the block
+        if(blocks[minIndex].file_no == 0) {
+            blocks[minIndex].file_no = i+1;
+            blocks[minIndex].file_size = file_size;
+            blocks[minIndex].fragmentation = blocks[minIndex].block_size - file_size;
         }
     }
 
